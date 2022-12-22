0191d8ac84c76d176c8e6d16ce2895d561d52ca6
Rongcui Dong <rongcuid@outlook.com>
2022-12-21 22:07:30 -0500

    MacOS build

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,11 +17,17 @@ val lwjglNatives = Pair(
 ).let { (name, arch) ->
     when {
         arrayOf("Linux", "FreeBSD", "SunOS", "Unit").any { name.startsWith(it) } ->
-            "natives-linux"
+            if (arrayOf("arm", "aarch64").any { arch.startsWith(it) })
+                "natives-linux${if (arch.contains("64") || arch.startsWith("armv8")) "-arm64" else "-arm32"}"
+            else
+                "natives-linux"
         arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) }                ->
-            "natives-macos"
+            "natives-macos${if (arch.startsWith("aarch64")) "-arm64" else ""}"
         arrayOf("Windows").any { name.startsWith(it) }                           ->
-            "natives-windows"
+            if (arch.contains("64"))
+                "natives-windows${if (arch.startsWith("aarch64")) "-arm64" else ""}"
+            else
+                "natives-windows-x86"
         else -> throw Error("Unrecognized or unsupported platform. Please set \"lwjglNatives\" manually")
     }
 }
@@ -59,20 +65,20 @@ dependencies {
     implementation("org.lwjgl", "lwjgl-opencl")
     implementation("org.lwjgl", "lwjgl-opengl")
     implementation("org.lwjgl", "lwjgl-opengles")
-    implementation("org.lwjgl", "lwjgl-openvr")
-    implementation("org.lwjgl", "lwjgl-openxr")
-    implementation("org.lwjgl", "lwjgl-opus")
-    implementation("org.lwjgl", "lwjgl-ovr")
+//    implementation("org.lwjgl", "lwjgl-openvr")
+//    implementation("org.lwjgl", "lwjgl-openxr")
+//    implementation("org.lwjgl", "lwjgl-opus")
+//    implementation("org.lwjgl", "lwjgl-ovr")
     implementation("org.lwjgl", "lwjgl-par")
     implementation("org.lwjgl", "lwjgl-remotery")
     implementation("org.lwjgl", "lwjgl-rpmalloc")
     implementation("org.lwjgl", "lwjgl-shaderc")
     implementation("org.lwjgl", "lwjgl-spvc")
-    implementation("org.lwjgl", "lwjgl-sse")
+//    implementation("org.lwjgl", "lwjgl-sse")
     implementation("org.lwjgl", "lwjgl-stb")
     implementation("org.lwjgl", "lwjgl-tinyexr")
     implementation("org.lwjgl", "lwjgl-tinyfd")
-    implementation("org.lwjgl", "lwjgl-tootle")
+//    implementation("org.lwjgl", "lwjgl-tootle")
     implementation("org.lwjgl", "lwjgl-vma")
     implementation("org.lwjgl", "lwjgl-vulkan")
     implementation("org.lwjgl", "lwjgl-xxhash")
@@ -95,22 +101,22 @@ dependencies {
     runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
     runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
     runtimeOnly("org.lwjgl", "lwjgl-opengles", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-openvr", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-openxr", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-opus", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-ovr", classifier = lwjglNatives)
+//    runtimeOnly("org.lwjgl", "lwjgl-openvr", classifier = lwjglNatives)
+//    runtimeOnly("org.lwjgl", "lwjgl-openxr", classifier = lwjglNatives)
+//    runtimeOnly("org.lwjgl", "lwjgl-opus", classifier = lwjglNatives)
+//    runtimeOnly("org.lwjgl", "lwjgl-ovr", classifier = lwjglNatives)
     runtimeOnly("org.lwjgl", "lwjgl-par", classifier = lwjglNatives)
     runtimeOnly("org.lwjgl", "lwjgl-remotery", classifier = lwjglNatives)
     runtimeOnly("org.lwjgl", "lwjgl-rpmalloc", classifier = lwjglNatives)
     runtimeOnly("org.lwjgl", "lwjgl-shaderc", classifier = lwjglNatives)
     runtimeOnly("org.lwjgl", "lwjgl-spvc", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-sse", classifier = lwjglNatives)
+//    runtimeOnly("org.lwjgl", "lwjgl-sse", classifier = lwjglNatives)
     runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
     runtimeOnly("org.lwjgl", "lwjgl-tinyexr", classifier = lwjglNatives)
     runtimeOnly("org.lwjgl", "lwjgl-tinyfd", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-tootle", classifier = lwjglNatives)
+//    runtimeOnly("org.lwjgl", "lwjgl-tootle", classifier = lwjglNatives)
     runtimeOnly("org.lwjgl", "lwjgl-vma", classifier = lwjglNatives)
-    if (lwjglNatives == "natives-macos") runtimeOnly("org.lwjgl", "lwjgl-vulkan", classifier = lwjglNatives)
+    if (lwjglNatives == "natives-macos" || lwjglNatives == "natives-macos-arm64") runtimeOnly("org.lwjgl", "lwjgl-vulkan", classifier = lwjglNatives)
     runtimeOnly("org.lwjgl", "lwjgl-xxhash", classifier = lwjglNatives)
     runtimeOnly("org.lwjgl", "lwjgl-yoga", classifier = lwjglNatives)
     runtimeOnly("org.lwjgl", "lwjgl-zstd", classifier = lwjglNatives)
